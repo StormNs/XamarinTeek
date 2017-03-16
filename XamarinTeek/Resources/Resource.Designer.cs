efeb35d8a1dc497bebbe788fa57c7fcf7577bbba
SONPHSE61822\StormNs <sonphse61822@fpt.edu.vn>
2017-03-16 21:26:08 +0700

    Create UI for login page

--- XamarinTeek/Resources/Resource.Designer.cs
+++ XamarinTeek/Resources/Resource.Designer.cs
@@ -47,12 +47,43 @@ namespace XamarinTeek
 			// aapt resource value: 0x7f020000
 			public const int Icon = 2130837504;
 			
+			// aapt resource value: 0x7f020001
+			public const int logo = 2130837505;
+			
 			static Drawable()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
 			}
 			
 			private Drawable()
+			{
+			}
+		}
+		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f050004
+			public const int btnFBSignin = 2131034116;
+			
+			// aapt resource value: 0x7f050003
+			public const int btnSignin = 2131034115;
+			
+			// aapt resource value: 0x7f050002
+			public const int edtPassword = 2131034114;
+			
+			// aapt resource value: 0x7f050001
+			public const int edtUsername = 2131034113;
+			
+			// aapt resource value: 0x7f050000
+			public const int imageView1 = 2131034112;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
 			{
 			}
 		}
