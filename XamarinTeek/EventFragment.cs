9b81f00da7032de4e644a4045f88ddfa6d286825
john <john@DANHDCSE61904>
2017-03-21 01:39:50 +0700

    no message

--- XamarinTeek/EventFragment.cs
+++ XamarinTeek/EventFragment.cs
@@ -57,7 +57,7 @@ namespace XamarinTeek
 
             //end
 
-            View view = inflater.Inflate(Resource.Layout.Event, container, false);
+            View view = inflater.Inflate(Resource.Layout.EventDetail, container, false);
             var eventImg = Resource.Drawable.logo;
             var eventDescription = "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
                 + " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown"
